0e29ddc63a2604ce6bd2a2bad113d636f113ae20
Oliver <oandersen@udesa.edu.ar>
2022-09-01 10:43:51 -0300

    intro busqueda

---
+++ proyecto/src/components/Busqueda/Busqueda.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Busqueda extends Component {
+    contructor(props){
+    super(props)
+    this.state={
+        valor:''
+    }}
+
+render(){
+    return(
+        <form>
+            <input value={'hola'}/>
+        </form>
+    )
+}
+}
+export default Busqueda
