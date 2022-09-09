27d6ba941fe67ef206ea888edb1c5ac037e1c92e
Facundo Bory <facubory@MacBook-Pro-de-Facundo-2.local>
2022-09-09 18:05:03 -0300

    Agregue el logo de la pagina y segui con el header

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -1,29 +1,34 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './styles.css'
 
-function Header(){
+function Header() {
 
-    return(
+    return (
         <>
-        <header className='header'>
-            <nav>
-                <ul >
-                    <li >
-                        <Link to='/'>
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/favoritos'>
-                            Favoritos
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    </>
+            <header className='header'>
+                <a href="/" className="logo">
+                    <img className='logo' src="./img/logo.png" alt="Logo de la pagina" />
+                    <h2 className='nombre-empresa'>We Stream</h2>
+                </a>
+
+                <nav>
+                
+                    <Link to='/'>Home</Link>
+
+                    <Link to='/Favoritos'>Favoritos</Link>
+
+                </nav>
+
+               
+
+            </header>
+            <hr className="header-separador" />
+        </>
     )
 }
 
 export default Header;
+
+
+
