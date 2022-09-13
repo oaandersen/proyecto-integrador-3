f6110baf56abddff73b90ad5538f707bf996b01f
Oliver <oandersen@udesa.edu.ar>
2022-09-13 13:23:23 -0300

    creacion de carpetas css en los componentes

--- proyecto/src/components/Detalle/Detalle.js
+++ proyecto/src/components/Detalle/Detalle.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './styles.css'
 
 class Detalle extends Component {
     constructor(props){
--- proyecto/src/components/DetalleAlbum/DetalleAlbum.js
+++ proyecto/src/components/DetalleAlbum/DetalleAlbum.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './styles.css'
 
 class DetalleAlbum extends Component {
     constructor(props){
